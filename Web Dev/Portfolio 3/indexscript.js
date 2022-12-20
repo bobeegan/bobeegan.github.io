f62f7614a12eccb8a24a8cf4c04a0dc44352ed7a
bobeegan <tegan.motomal@gmail.com>
2022-12-20 17:49:18 -0500

    Portfolio V.3!

---
+++ Web Dev/Portfolio 3/indexscript.js
@@ -0,0 +1,34 @@
+
+//style switch
+function onClickStyle(style){
+    let darkButton = document.getElementById("dark");
+    let lightButton = document.getElementById("light");
+    let outer = document.getElementById("shell");
+    shell.setAttribute("class", style);
+    if(style==="indexDark"){
+        darkButton.style.display = "none";
+        lightButton.style.display = "";
+    }
+    else{
+        darkButton.style.display = "";
+        lightButton.style.display = "none";
+        
+    }
+}
+
+//email validation
+function onClickFeedback(){
+    let mail = document.getElementById("email");
+    let text = document.getElementById("text");
+
+    if(mail.value.length===0){
+        window.alert("Please enter an email");
+    }
+    else{
+        window.alert("Thank you for your submission!")
+    }
+}
+
+function onClickPrint(){
+    window.print();
+}
